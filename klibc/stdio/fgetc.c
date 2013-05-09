87c35b95a46fb52e6e11c4cc8b452e412cc68bd3
ntfc <ntfc30@gmail.com>
2013-05-09 17:38:14 +0100

    Changed unsigned char to char cast. Now it fails on a rte assert

--- klibc/stdio/fgetc.c
+++ klibc/stdio/fgetc.c
@@ -12,8 +12,9 @@
     assumes (stdio_pvt(file)->ibytes) > 0;
     assigns stdio_pvt(file)->ibytes, stdio_pvt(file)->data;
     ensures stdio_pvt(file)->ibytes == \at(stdio_pvt(file)->ibytes, Pre)-1;
-    ensures stdio_pvt(file)->data == \at(stdio_pvt(file)->data, Pre) +1;
-    ensures \result == (int)(*\at(stdio_pvt(file)->data,Pre));
+    ensures stdio_pvt(file)->data == \at(stdio_pvt(file)->data, Pre)+1;
+    //ensures \result == *(stdio_pvt(file)->data-1);
+    ensures \result == *(\at((stdio_pvt(file)->data), Pre));
   behavior error:
     assumes (stdio_pvt(file))->ibytes == 0;
     assigns \nothing;
@@ -25,11 +26,11 @@
 int fgetc(FILE *file)
 {
   struct _IO_file_pvt *f = stdio_pvt(file);
-  unsigned char ch;
+  /*unsigned */char ch;
 
-  if (/*__likely*/(f->ibytes)) {
+  if (__likely(f->ibytes)) {
     f->ibytes--;
-    return (unsigned char) *f->data++;
+    return (/*unsigned */char) *f->data++;
   } else {
     //return _fread(&ch, 1, file) == 1 ? ch : EOF;
     return EOF; // inserted code
