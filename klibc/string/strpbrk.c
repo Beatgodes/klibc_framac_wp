0b1ce53ca80f04717eb72ca3537d228b7e4c581c
ntfc <ntfc30@gmail.com>
2013-06-25 23:25:25 +0100

    PI: moved strpbrk contract to string.h. Some work done on strsep. Still missing something on the last behavior

--- klibc/string/strpbrk.c
+++ klibc/string/strpbrk.c
@@ -6,25 +6,6 @@
 
 #include "strxspn.h"
 
-/*@
-	requires \exists integer i; Length_of_str_is(s, i);
-	requires \exists integer i; Length_of_str_is(accept, i);
-	requires \separated(s+(0..Length(s)), accept+(0..Length(accept)));
-	assigns \nothing;
-
-	behavior exists:
-		assumes \exists integer i; 0 <= i < Length(s) && char_in_str(accept, s[i]);
-		ensures \forall integer i; 0 <= i < (\result - s) ==> !char_in_str(accept, s[i]);
-		ensures char_in_str(accept, *\result);
-
-	behavior not_exists:
-		assumes \forall integer i; 0 <= i < Length(s) ==> !char_in_str(accept, s[i]);
-		ensures \result == \null;
-
-	complete behaviors;
-	disjoint behaviors;
-
-@*/
 char *strpbrk(const char *s, const char *accept)
 {
 	const char *ss = s + __strxspn(s, accept, 1);
--- klibc/string/strsep.c
+++ klibc/string/strsep.c
@@ -4,6 +4,31 @@
 
 #include <string.h>
 
+/*@
+	behavior null:
+		assumes *stringp == \null;
+		assigns \nothing;
+		ensures \result == \null;
+	behavior no_delim_found:
+		assumes \exists integer i; Length_of_str_is(*stringp, i);
+		assumes \exists integer i; Length_of_str_is(delim, i);
+		assumes \forall integer i; 0 <= i < Length(*stringp) && !char_in_str(delim, (*stringp)[i]);
+		assigns *stringp; // proved with cvc3
+		ensures \result == \null; // proved with cvc3
+		ensures *stringp == \null; // proved with z3
+	behavior delim_found:
+		assumes \exists integer i; Length_of_str_is(*stringp, i);
+		assumes \exists integer i; Length_of_str_is(delim, i);
+		assumes \exists integer i; 0 <= i < Length(*stringp) && char_in_str(delim, (*stringp)[i]);
+		assigns *stringp;
+		ensures \result != \null;
+		ensures Length(\result) == Length(\old(*stringp)) - (\result - \old(*stringp));
+		ensures char_in_str(delim, *\result);
+		
+
+	complete behaviors;
+	disjoint behaviors;
+@*/
 char *strsep(char **stringp, const char *delim)
 {
 	char *s = *stringp;
@@ -13,9 +38,11 @@ char *strsep(char **stringp, const char *delim)
 		return NULL;
 
 	e = strpbrk(s, delim);
+
 	if (e)
 		*e++ = '\0';
 
 	*stringp = e;
+	
 	return s;
 }
