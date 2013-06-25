19f62a2fef91e8008ea9f6fb56489352cca10122
ntfc <ntfc30@gmail.com>
2013-06-25 23:34:38 +0100

    PI: added separated clause to strsep

--- klibc/string/strsep.c
+++ klibc/string/strsep.c
@@ -13,6 +13,7 @@
 		assumes \exists integer i; Length_of_str_is(*stringp, i);
 		assumes \exists integer i; Length_of_str_is(delim, i);
 		assumes \forall integer i; 0 <= i < Length(*stringp) && !char_in_str(delim, (*stringp)[i]);
+		assumes \separated(delim+(0..Length(delim)), *stringp+(0..Length(*stringp)));
 		assigns *stringp; // proved with cvc3
 		ensures \result == \null; // proved with cvc3
 		ensures *stringp == \null; // proved with z3
@@ -20,6 +21,7 @@
 		assumes \exists integer i; Length_of_str_is(*stringp, i);
 		assumes \exists integer i; Length_of_str_is(delim, i);
 		assumes \exists integer i; 0 <= i < Length(*stringp) && char_in_str(delim, (*stringp)[i]);
+		assumes \separated(delim+(0..Length(delim)), *stringp+(0..Length(*stringp)));
 		assigns *stringp;
 		ensures \result != \null;
 		ensures Length(\result) == Length(\old(*stringp)) - (\result - \old(*stringp));
