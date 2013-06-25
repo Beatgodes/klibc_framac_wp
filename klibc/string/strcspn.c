bcc85016d49f0b2a57db090dd29989ebb966a0b6
Cristiano Sousa <beatgodes@93.102.239.162.rev.optimus.pt>
2013-06-25 20:38:18 +0100

    PI: strcspn and strspn done

--- klibc/string/strcspn.c
+++ klibc/string/strcspn.c
@@ -6,6 +6,16 @@
 
 #include "strxspn.h"
 
+/*@
+	requires \exists integer i; Length_of_str_is(s,i);
+	requires \exists integer i; Length_of_str_is(reject,i);
+	requires \separated(s+(0..Length(s)), reject+(0..Length(reject)));
+	assigns \nothing;
+	ensures \forall integer i; 0 <= i < \result ==> !char_in_str(reject, s[i]);
+	ensures char_in_str(reject, s[\result]);
+	ensures 0 <= \result <= Length(s);
+
+@*/
 size_t strcspn(const char *s, const char *reject)
 {
 	return __strxspn(s, reject, 1);
--- klibc/string/strspn.c
+++ klibc/string/strspn.c
@@ -6,6 +6,15 @@
 
 #include "strxspn.h"
 
+/*@
+	requires \exists integer i; Length_of_str_is(s,i);
+	requires \exists integer i; Length_of_str_is(accept,i);
+	requires \separated(s+(0..Length(s)), accept+(0..Length(accept)));
+	assigns \nothing;
+	ensures \forall integer i; 0 <= i < \result ==> char_in_str(accept, s[i]);
+	ensures !char_in_str(accept, s[\result]);
+	ensures 0 <= \result <= Length(s);
+@*/
 size_t strspn(const char *s, const char *accept)
 {
 	return __strxspn(s, accept, 0);
